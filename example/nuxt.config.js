c17969ad85f22ae6c4a0248ecc19791b8b3b6bf9
Steven Ho <shockshocksoul@gmail.com>
2019-10-20 15:29:33 +0800

    ** Update ** DEMO site.

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -10,6 +10,15 @@ const routerBase =
     : {}
 
 module.exports = {
+
+  head: {
+    title: 'Nuxt-Breakpoints',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Resize observer breakpoints with Nuxt.js module.' }
+    ]
+  },
   rootDir: resolve(__dirname, '..'),
   buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
